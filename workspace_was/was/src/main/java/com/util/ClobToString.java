4219a39e1c45bb25af96d1849bb111ef2bd3f628
1sinjn <1sinjn@gmail.com>
2019-11-02 20:37:44 +0900

    최신화

--- workspace_was/was/src/main/java/com/util/ClobToString.java
+++ workspace_was/was/src/main/java/com/util/ClobToString.java
@@ -4,18 +4,6 @@ import java.io.BufferedReader;
 import java.sql.Clob;
 
 public class ClobToString {
-<<<<<<< HEAD
-	 public static String clobToString(Clob clob) throws Exception {
-	        StringBuffer s = new StringBuffer();
-	        BufferedReader br = new BufferedReader(clob.getCharacterStream());
-	        String ts = "";
-	        while((ts = br.readLine()) != null) {
-	               s.append(ts + "\n");
-	        }
-	        br.close();
-	        return s.toString();
-	 }
-=======
     public String clobToString(Clob clob) throws Exception {
            StringBuffer s = new StringBuffer();
            BufferedReader br = new BufferedReader(clob.getCharacterStream());
@@ -26,5 +14,4 @@ public class ClobToString {
            br.close();
            return s.toString();
     }
->>>>>>> origin/master
 }
